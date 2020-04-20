fa31bc1aaf57b2e272c38b78c9250cbb67a91f33
Negei <szraat@gmail.com>
2020-04-20 19:26:05 +0200

    aaa

--- wp_main.php
+++ wp_main.php
@@ -2,37 +2,34 @@
 <html>
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="css_style.css" type="text/css">
   <title>main</title>
 </head>
 
-<?php $activePage = basename($_SERVER['PHP_SELF'], ".php");?>
-<?php 
-	$directoryURI = $_SERVER['REQUEST_URI'];
-	$path = parse_url($directoryURI, PHP_URL_QUERY);
-?>
+<header>
+	<div class="headerleft">
+		<a> Simple webpage</a>
+	</div>
+	<div class="headerright">
+		<a> login </a>
+	</div>
+</header>
+
 <body>
-  <div class="header">
-    <br>
-	Simple webpage
-  </div>
+<?php $page = isset($_GET['page']) ? $_GET['page'] : '1';?>
   <div class='menu'>
-	<ul>
-		<li class="<?php if ($path == "page=1") {echo "active"; } else  {echo "noactive";}?>"><a href="wp_main.php?page=1">Home</a></li>
-		<li class="<?php if ($path == "page=2") {echo "active"; } else  {echo "noactive";}?>"><a href="wp_main.php?page=1"><a href="wp_main.php?page=2">HTML basic</a></li>
-		<li class="<?php if ($path == "page=3") {echo "active"; } else  {echo "noactive";}?>"><a href="wp_main.php?page=1"><a href="wp_main.php?page=3">CSS basic</a></li>
-		<li><a href="wp_main.php?page=1"><a href="wp_main.php?page=4">PHP basic</a></li>
-	</ul>
+		<a class="<?php if ($page==1) {echo "active"; } else  {echo "noactive";}?>" href="wp_main.php?page=1"><i class="fa fa-fw fa-home"></i><i class="fas fa-home"></i>Home</a>
+		<a class="<?php if ($page==2) {echo "active"; } else  {echo "noactive";}?>" href="wp_main.php?page=2">HTML basic</a>
+		<a class="<?php if ($page==3) {echo "active"; } else  {echo "noactive";}?>" href="wp_main.php?page=3">CSS basic</a>
+		<a class="<?php if ($page==4) {echo "active"; } else  {echo "noactive";}?>" href="wp_main.php?page=4">PHP basic</a></li>	
   </div>
   <div class="tartalom">
 	<?php
-	header("Content-type: text/html;charset='UTF-8'");
-    $page = $_GET["page"];
-    if($page==1)     include("wp_home.php");
-    elseif($page==2) include("wp_html_basic.php");
-    elseif($page==3) include("wp_css_basic.php");
-    elseif($page==4) include("wp_php_basic.php");
-	print($path);
+		if($page==1)     include("wp_home.php");
+		elseif($page==2) include("wp_html_basic.php");
+		elseif($page==3) include("wp_css_basic.php");
+		elseif($page==4) include("wp_php_basic.php");
      ?>
   </div>
 </body>
